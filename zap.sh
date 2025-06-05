a3aa21aee1623569c98681a5f210fcb6e4a5457e
Gumbeoketch <okellogumbe@gmail.com>
2025-06-05 22:47:01 +0300

    new zap.sh

--- zap.sh
+++ zap.sh
@@ -1,47 +1,33 @@
 #!/bin/bash
 
-# ==== USER INPUTS OR ENV VARS (set these) ====
-serviceName="devsecops-svc"
-applicationURL="http://13.246.61.247"
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
-# ==== Extract NodePort safely ====
-PORT=$(kubectl -n default get svc ${serviceName} -o json | jq '.spec.ports[] | select(.port==8080) | .nodePort')
-
-if [[ -z "$PORT" ]]; then
-  echo "Failed to get NodePort. Exiting."
-  exit 1
-fi
-
-echo "Scanning target: $applicationURL:$PORT"
-
-# ==== File permissions ====
+# first run this
 chmod 777 $(pwd)
+echo $(id -u):$(id -g)
+# docker run -v $(pwd):/zap/wrk/:rw -t ghcr.io/zaproxy/zaproxy:weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
-# ==== Run ZAP scan ====
-docker pull ghcr.io/zaproxy/zaproxy:weekly
 
-docker run --rm -v $(pwd):/zap/wrk/:rw \
-  -t ghcr.io/zaproxy/zaproxy:weekly \
-  zap-api-scan.py -t "$applicationURL:$PORT/v3/api-docs" \
-  -f openapi -c zap_rules -r zap_report.html
+# comment above cmd and uncomment below lines to run with CUSTOM RULES
+docker run -v $(pwd):/zap/wrk/:rw -t ghcr.io/zaproxy/zaproxy:weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
 
 exit_code=$?
 
-# ==== Move report if generated ====
-if [[ -f zap_report.html ]]; then
-  mkdir -p owasp-zap-report
-  mv zap_report.html owasp-zap-report/
-else
-  echo "ZAP report not found!"
-  exit 1
-fi
 
-# ==== Handle exit code ====
+# HTML Report
+ sudo mkdir -p owasp-zap-report
+ sudo mv zap_report.html owasp-zap-report
+
+
 echo "Exit Code : $exit_code"
 
-if [[ ${exit_code} -ne 0 ]]; then
-  echo "OWASP ZAP reported risks. Check the report."
-  exit 1
-else
-  echo "OWASP ZAP did not report any Risk."
-fi
+ if [[ ${exit_code} -ne 0 ]];  then
+    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+    exit 1;
+   else
+    echo "OWASP ZAP did not report any Risk"
+ fi;
+
+
+# Generate ConfigFile
+# docker run -v $(pwd):/zap/wrk/:rw -t ghcr.io/zaproxy/zaproxy:weekly zap-api-scan.py -t http://devsecops-demo.eastus.cloudapp.azure.com:31933/v3/api-docs -f openapi -g gen_file
