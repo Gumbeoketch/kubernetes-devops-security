cd6d82b78e9ca55815dd373c87ca4827627556bd
Michael Oketch <48472370+Gumbeoketch@users.noreply.github.com>
2026-04-16 21:35:40 +0300

    trivy

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -4,8 +4,8 @@ dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile) #Getting docker base image 
 echo $dockerImageName
 
 #Running Using Trivy Docker Image
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.69.3 -q image --exit-code 0 --severity HIGH --light $dockerImageName #Ignore any HIGH (light parameter on trivy to show less details)
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.69.3 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName #Stop build on CRITICAL
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName #Ignore any HIGH (light parameter on trivy to show less details)
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName #Stop build on CRITICAL
 
     # Trivy scan result processing
     exit_code=$?
